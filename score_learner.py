5b4f8504e279c0dd22f6e9bccccec2dd67019658
MElahi <elahi.mojtaba@gmail.com>
2018-04-21 10:06:23 +0430

    Creating database reader and add predict feature to our model.

--- score_learner.py
+++ score_learner.py
@@ -6,34 +6,35 @@ import os
 import cv2
 
 
+class ScoreImageReader:
+    @staticmethod
+    def __reading_images(path):
+        features = []
+        labels = []
+        if path is None:
+            print("Error: There is no path for reading images")
+        else:
+            for filename in os.listdir(path):
+                labels.append(filename.split(sep='.png')[0])
+                features.append(cv2.imread(os.path.join(path, filename)))
 
+        return np.asarray(features, dtype=np.float16), np.asarray(labels, dtype=np.int32)
 
-def reading_images(path):
-    features = []
-    labels = []
-    for filename in os.listdir(path):
-        labels.append(filename.split(sep='.png')[0])
-        features.append(cv2.imread(os.path.join(path, filename)))
-    return np.asarray(features, dtype=np.float16), np.asarray(labels, dtype=np.int32)
-
-
-def my_dataset(features, labels):
-    training_dataset = tf.data.Dataset.from_tensor_slices((features, labels))
-    return training_dataset.shuffle(30).batch(10).repeat()
+    def dataset_from_file(self, path, repeat_count=None):
+        features, labels = self.__reading_images(path)
+        training_dataset = tf.data.Dataset.from_tensor_slices((features, labels))
+        return training_dataset.shuffle(30).batch(10).repeat(repeat_count)
 
 
 class ScoreReader:
     def __init__(self):
         self.number_of_conv_filters = 8
         self.dense_units = 50
-        self.model = tf.estimator.Estimator(model_fn=self.model_fn, model_dir="./score_reader")
+        output = "score_reader_{}_{}".format(self.number_of_conv_filters, self.dense_units)
+        self.model = tf.estimator.Estimator(model_fn=self.model_fn, model_dir=output)
         self.batch_size = 10
         self.steps = 1000
-        self.training_dataset = None
-
-    def preparing_training_dataset(self, features, labels):
-        self.training_dataset = tf.data.Dataset.from_tensor_slices((features, labels))
-        self.training_dataset.shuffle(30).batch(self.batch_size)
+        self.image_reader = ScoreImageReader()
 
     def model_fn(self, features, labels, mode):
         net = tf.reshape(features, [-1, 20, 20, 3])
@@ -64,22 +65,22 @@ class ScoreReader:
         }
         return tf.estimator.EstimatorSpec(mode=mode, eval_metric_ops=eval_metrics, loss=loss)
 
-    def training(self, features, labels):
-        self.training_dataset = self.training_dataset.repeat()
-        print(self.training_dataset)
-        print(my_dataset(features, labels))
-        self.preparing_training_dataset(features, labels)
-        self.model.train(input_fn=lambda: self.training_dataset, steps=self.steps)
-        evaluation = self.model.evaluate(input_fn=lambda: self.training_dataset.repeat(20))
+    def training(self, path):
+        self.model.train(input_fn=lambda: self.image_reader.dataset_from_file(path), steps=self.steps)
+        evaluation = self.model.evaluate(input_fn=lambda: self.image_reader.dataset_from_file(path, 20))
         print(evaluation)
+
+    def predicting(self, path):
+        predictions = self.model.predict(input_fn=lambda: self.image_reader.dataset_from_file(path, 1))
+        for prediction in predictions:
+            print(prediction)
 
 
 def main():
     print("initializing")
-    features, labels = reading_images("scores")
     score_reader = ScoreReader()
-    score_reader.preparing_training_dataset(features, labels)
-    score_reader.training(features, labels)
+    score_reader.training(path="./scores/")
+    score_reader.predicting(path="./scores/")
 
 
 if __name__ == "__main__":
